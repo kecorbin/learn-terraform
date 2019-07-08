422a29447a302e766ffaab420c565d6372647a36
Kevin Corbin <kcorbin@hashicorp.com>
2019-07-08 12:51:43 -0500

    add security group

--- aws.tf
+++ aws.tf
@@ -18,6 +18,20 @@ resource "aws_eip" "ip" {
   instance = aws_instance.example.id
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "0.0.0.0/0"
+  }
+}
+
 output "ip" {
   value = aws_eip.ip.public_ip
 }

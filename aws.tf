9045a9b4b3cd96e0afa65c030680ed9e86307a8e
Kevin Corbin <kcorbin@hashicorp.com>
2019-07-08 12:56:26 -0500

    update proto

--- aws.tf
+++ aws.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_ssh" {
     # TLS (change to whatever ports you need)
     from_port   = 22
     to_port     = 22
-    protocol    = "-1"
+     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]

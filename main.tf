71de1ab4622a4b41f8e9db8e6f1e1e97844f2d95
Kevin Corbin <kcorbin@hashicorp.com>
2019-07-12 14:41:08 -0500

    remove google provider

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-provider "google" {
-  credentials = "${var.gcp_credentials}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
-}
+// provider "google" {
+//   credentials = "${var.gcp_credentials}"
+//   project     = "${var.gcp_project}"
+//   region      = "${var.gcp_region}"
+// }
